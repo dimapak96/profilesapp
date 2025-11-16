865e7e30549f00af73d52dff129df67b3213ffb7
dimapak96 <dmitriy.p.a.96@gmail.com>
2025-11-15 23:34:22 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import './App.css';
-
 function App() {
   return (
-    <div
-      style={{
-        textAlign: 'center',
-        marginTop: '100px',
-        fontFamily: 'Arial',
-      }}
-    >
+    <div>
       <h1>Hello, my name is Dmitriy Pak ⭐</h1>
+
       <p>This is my first deployed React + AWS Amplify website.</p>
 
-      <p>
-        <strong>Made by Dmitriy Pak</strong>
-      </p>
+      <p><strong>Made by Dmitriy Pak</strong></p>
 
       <p>
-        <a
-          href="https://github.com/dimapak96"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://github.com/dimapak96" target="_blank" rel="noreferrer">
           Visit my GitHub
         </a>
       </p>
