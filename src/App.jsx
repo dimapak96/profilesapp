5721ff6d234b0b724438797043d09366673a4104
dimapak96 <dmitriy.p.a.96@gmail.com>
2025-11-15 23:27:19 -0500

    Updated homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div
+      style={{
+        textAlign: 'center',
+        marginTop: '100px',
+        fontFamily: 'Arial',
+      }}
+    >
+      <h1>Hello, my name is Dmitriy Pak ⭐</h1>
+      <p>This is my first deployed React + AWS Amplify website.</p>
+
+      <p>
+        <strong>Made by Dmitriy Pak</strong>
       </p>
-    </>
-  )
+
+      <p>
+        <a
+          href="https://github.com/dimapak96"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Visit my GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
